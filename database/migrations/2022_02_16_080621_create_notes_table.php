8c83fa738bdb4af05e3be2623a5e2e6ef43fbf30
lReymarkANM <77568046+lReymarkANM@users.noreply.github.com>
2022-04-18 13:49:09 +0800

    Add files via upload

---
+++ database/migrations/2022_02_16_080621_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('collection_id');
+            $table->foreign('collection_id')->references('id')->on('collections')->onDelete('cascade');
+            $table->string('title')->default('Untitled');
+            $table->longtext('content')->nullable();
+            $table->integer('target_word_count')->default(0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
